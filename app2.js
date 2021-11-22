d257580aa5f81be545147dfee47a365215775919
Pmastra <pabloo.agustin@gmail.com>
2021-11-22 14:45:22 -0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -5,19 +5,19 @@ const port = 3000;
 app.set('view engine', 'html');
 
 app.get("/", (req, res) => {
-  res.send("index");
+    res.send("index");
 });
 
 app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(__dirname + "/public/404.html");
+    res.status(404).sendFile(__dirname + "/public/api/404.html");
 });
 
 app.get("/contacto", (req, res) => {
-  res.send(__dirname);
+    res.send(__dirname);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
