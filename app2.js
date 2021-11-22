dd98d713115b2d8063e1873002b8362a5bc299c4
Pmastra <pabloo.agustin@gmail.com>
2021-11-22 16:03:56 -0300

    Delete Rutas

--- app2.js
+++ app2.js
@@ -6,7 +6,13 @@ app.set('view engine', 'html');
 
 app.use(express.static(__dirname + "/public"));
 
-app.use('/', require('./router/rutas'));
+app.get("/", (req, res) => {
+    res.send("index");
+});
+
+app.get("/contacto", (req, res) => {
+    res.send(__dirname);
+});
 
 app.use((req, res, next) => {
     res.status(404).sendFile(__dirname + "/public/api/404.html");
