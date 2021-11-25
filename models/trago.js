08fe6b118f8f8ccbe2f4c3965c02a0f9b4610ae2
Pmastra <pabloo.agustin@gmail.com>
2021-11-24 23:26:56 -0300

    Update schema

--- models/trago.js
+++ models/trago.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const tragoSchema = new Schema ({
+const tragoSchema = new Schema({
     "idDrink": String,
     "strDrink": String,
     "strCategory": String,
     "strAlcoholic": String,
     "strInstructions": String,
     "strDrinkThumb": String,
-    "ingredientes": {
-        "strIngredient1": String,
-        "strIngredient2": String,
-        "strIngredient3": String}
+    "strIngredient1": String,
+    "strIngredient2": String,
+    "strIngredient3": String,
+    "strIngredient4": String,
+    "strIngredient5": String,
+    "strIngredient6": String
 })
 
 //crear modelo
